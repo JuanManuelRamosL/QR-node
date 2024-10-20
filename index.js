666b9dc54b047780f2a3473b2ea814e3c853f714
juanmanuel <juanmanuelramoslencina@gmail.com>
2024-10-19 23:31:59 -0300

    url imagen

--- index.js
+++ index.js
@@ -247,7 +247,7 @@ app.get('/confirm', (req, res) => {
             justify-content: center;
             align-items: center;
             height: 100vh;
-            background-image: url('portada1.jpg');
+            background-image: url('/PORTADA1.jpg');
             background-size: cover;
             background-position: center;
           }
